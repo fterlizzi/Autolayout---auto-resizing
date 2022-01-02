96fc3f14b6c758fc9c5b84741b1065b693e7a9ee
fterlizzi <fterlizzi@gmail.com>
2022-01-02 08:34:41 +0100

    Initial Commit

---
+++ Autolayout - auto resizing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autolayout - auto resizing
+//
+//  Created by Francesco terlizzi on 02/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
